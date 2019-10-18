1f23854d04b4368d6292bcc241506933b5260a00
Unknown <mlund@itba.edu.ar>
2019-10-18 19:41:23 -0300

    Add model classes

---
+++ tpe2/api/src/main/java/ar/edu/itba/pod/model/Airport.java
@@ -0,0 +1,146 @@
+package ar.edu.itba.pod.model;
+
+public class Airport {
+    private final String localDesignator;
+    private final String OACIDesignator;
+    private final String IATADesignator;
+    private final String type;
+    private final String name;
+    private final String coordinates;
+    private final double latitude;
+    private final double longitude;
+    private final double altitude;
+    private final String altitudeUnit;
+    private final String referenceCity;
+    private final double referenceDistance;
+    private final String referenceDirection;
+    private final String condition;
+    private final String control;
+    private final String region;
+    private final String FIR;
+    private final String usage;
+    private final String traffic;
+    private final String SNA;
+    private final String concessioned;
+    private final String province;
+    private final String inhabilitated;
+
+    public Airport(String localDesignator, String OACIDesignator, String IATADesignator, String type, String name, String coordinates, double latitude, double longitude, double altitude, String altitudeUnit, String referenceCity, double referenceDistance, String referenceDirection, String condition, String control, String region, String FIR, String usage, String traffic, String SNA, String concessioned, String province, String inhabilitated) {
+        this.localDesignator = localDesignator;
+        this.OACIDesignator = OACIDesignator;
+        this.IATADesignator = IATADesignator;
+        this.type = type;
+        this.name = name;
+        this.coordinates = coordinates;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.altitude = altitude;
+        this.altitudeUnit = altitudeUnit;
+        this.referenceCity = referenceCity;
+        this.referenceDistance = referenceDistance;
+        this.referenceDirection = referenceDirection;
+        this.condition = condition;
+        this.control = control;
+        this.region = region;
+        this.FIR = FIR;
+        this.usage = usage;
+        this.traffic = traffic;
+        this.SNA = SNA;
+        this.concessioned = concessioned;
+        this.province = province;
+        this.inhabilitated = inhabilitated;
+    }
+
+    public String getLocalDesignator() {
+        return localDesignator;
+    }
+
+    public String getOACIDesignator() {
+        return OACIDesignator;
+    }
+
+    public String getIATADesignator() {
+        return IATADesignator;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCoordinates() {
+        return coordinates;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getAltitude() {
+        return altitude;
+    }
+
+    public String getAltitudeUnit() {
+        return altitudeUnit;
+    }
+
+    public String getReferenceCity() {
+        return referenceCity;
+    }
+
+    public double getReferenceDistance() {
+        return referenceDistance;
+    }
+
+    public String getReferenceDirection() {
+        return referenceDirection;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public String getControl() {
+        return control;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public String getFIR() {
+        return FIR;
+    }
+
+    public String getUsage() {
+        return usage;
+    }
+
+    public String getTraffic() {
+        return traffic;
+    }
+
+    public String getSNA() {
+        return SNA;
+    }
+
+    public String getConcessioned() {
+        return concessioned;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public String getInhabilitated() {
+        return inhabilitated;
+    }
+
+}
---
+++ tpe2/api/src/main/java/ar/edu/itba/pod/model/Movement.java
@@ -0,0 +1,61 @@
+package ar.edu.itba.pod.model;
+
+public class Movement {
+    private final String movementDate;
+    private final String movementTime;
+    private final String flightType;
+    private final String flightClassification;
+    private final String movementType;
+    private final String OACIOrigin;
+    private final String OACIDestination;
+    private final String airlineName;
+    private final String airshipName;
+
+    public Movement(String movementDate, String movementTime, String flightType, String flightClassification, String movementType, String OACIOrigin, String OACIDestination, String airlineName, String airshipName) {
+        this.movementDate = movementDate;
+        this.movementTime = movementTime;
+        this.flightType = flightType;
+        this.flightClassification = flightClassification;
+        this.movementType = movementType;
+        this.OACIOrigin = OACIOrigin;
+        this.OACIDestination = OACIDestination;
+        this.airlineName = airlineName;
+        this.airshipName = airshipName;
+    }
+
+    public String getMovementDate() {
+        return movementDate;
+    }
+
+    public String getMovementTime() {
+        return movementTime;
+    }
+
+    public String getFlightType() {
+        return flightType;
+    }
+
+    public String getFlightClassification() {
+        return flightClassification;
+    }
+
+    public String getMovementType() {
+        return movementType;
+    }
+
+    public String getOACIOrigin() {
+        return OACIOrigin;
+    }
+
+    public String getOACIDestination() {
+        return OACIDestination;
+    }
+
+    public String getAirlineName() {
+        return airlineName;
+    }
+
+    public String getAirshipName() {
+        return airshipName;
+    }
+}
