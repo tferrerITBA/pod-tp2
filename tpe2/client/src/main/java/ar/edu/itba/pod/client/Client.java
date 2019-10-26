e47d7b44b6aac1027e442042ff2aed812868b1c8
johnathan <jkatan@itba.edu.ar>
2019-10-26 16:01:30 -0300

    Add hazelcast client configuration

--- tpe2/client/src/main/java/ar/edu/itba/pod/client/Client.java
+++ tpe2/client/src/main/java/ar/edu/itba/pod/client/Client.java
@@ -1,5 +1,9 @@
 package ar.edu.itba.pod.client;
 
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,5 +12,16 @@ public class Client {
 
     public static void main(String[] args) {
         logger.info("tpe2 Client Starting ...");
+
+        //Example from the documentation
+
+        ClientConfig clientConfig = new ClientConfig();
+        clientConfig.getGroupConfig().setName("g5").setPassword("12345678");
+        clientConfig.getNetworkConfig().addAddress("192.168.1.18");
+
+        HazelcastInstance client = HazelcastClient.newHazelcastClient( clientConfig );
+
+        IMap map = client.getMap( "customers" );
+        System.out.println( "Map Size:" + map.size() );
     }
 }
--- tpe2/server/src/main/java/ar/edu/itba/pod/server/Server.java
+++ tpe2/server/src/main/java/ar/edu/itba/pod/server/Server.java
@@ -1,25 +1,39 @@
 package ar.edu.itba.pod.server;
 
+import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
+import java.util.Queue;
 
 public class Server {
     private static Logger logger = LoggerFactory.getLogger(Server.class);
 
     public static void main(String[] args) {
         logger.info("tpe2 Server Starting ...");
-        HazelcastInstance hz = Hazelcast.newHazelcastInstance();
-        Map<String, String> datos = hz.getMap("g5-materias");
-        datos.put("72.42", "POD");
-        System.out.println(String.format("%d Datos en el cluster", datos.size()));
 
-        for (String key : datos.keySet()) {
-            System.out.println(String.format("Datos con key %s= %s", key,
-                    datos.get(key)));
-        }
+        //Example from the documentation
+
+       Config cfg = new Config();
+       cfg.getGroupConfig().setName("g5").setPassword("12345678");
+        HazelcastInstance instance = Hazelcast.newHazelcastInstance(cfg);
+        Map<Integer, String> mapCustomers = instance.getMap("customers");
+        mapCustomers.put(1, "Joe");
+        mapCustomers.put(2, "Ali");
+        mapCustomers.put(3, "Avi");
+
+        System.out.println("Customer with key 1: "+ mapCustomers.get(1));
+        System.out.println("Map Size:" + mapCustomers.size());
+
+        Queue<String> queueCustomers = instance.getQueue("customers");
+        queueCustomers.offer("Tom");
+        queueCustomers.offer("Mary");
+        queueCustomers.offer("Jane");
+        System.out.println("First customer: " + queueCustomers.poll());
+        System.out.println("Second customer: "+ queueCustomers.peek());
+        System.out.println("Queue size: " + queueCustomers.size());
     }
 }
