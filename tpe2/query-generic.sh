67cbd32bc053883b0958fe624393626b659c4c2e
tferrerITBA <tferrer@itba.edu.ar>
2019-11-03 00:44:37 -0300

    Make IP Range configurable in hazelcast.xml

--- tpe2/query-generic.sh
+++ tpe2/query-generic.sh
@@ -2,5 +2,5 @@
 
 function query() {
 
-	java ${@:2} -Dquery=$1 -DLOG_FILE=query$1.txt -cp 'client/target/tpe2-client-1.0-SNAPSHOT/lib/jars/*' "ar.edu.itba.pod.client.Client"
+	java "${@:2}" -Dquery="$1" -DLOG_FILE="query$1.txt" -cp 'client/target/tpe2-client-1.0-SNAPSHOT/lib/jars/*' "ar.edu.itba.pod.client.Client"
 }
